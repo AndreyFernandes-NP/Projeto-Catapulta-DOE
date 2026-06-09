ca85c69eabf0b1f6d13b073041540b90893bb5f4
Andrey <andreyfernandes1361@gmail.com>
2026-06-08 22:34:00 -0300

    [andrey+pedro] main app class init

--- src/__main__.py
+++ src/__main__.py
@@ -5,9 +5,11 @@ ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT))
 
 from src.data_loader import get_csv, load_csv
+from src.supervised import Catapulta
 
 if __name__ == "__main__":
     fontes = get_csv()
+    cat_cls = Catapulta()
 
     if not fontes:
         raise FileNotFoundError("[Erro] Nenhum arquivo CSV encontrado em 'data/'")
@@ -15,8 +17,11 @@ if __name__ == "__main__":
     for fonte in fontes:
         try:
             df = load_csv(fonte)
+            cat_cls.store_dataset(fonte["nome"], df)
             print(f"'{fonte['nome']}' carregado com {len(df)} linhas e {len(df.columns)} colunas.") # Remover depois
             # TODO: Exploração inicial dos dados, como tipos de colunas, estatísticas básicas, .head(), e etc.
             # TODO: Fazer ajuste de curvas e resolver otimização
         except Exception as e:
             print(f"Erro ao carregar '{fonte['nome']}': {e}")
+    
+    print(f"Fontes disponíveis: {cat_cls.get_fonts()}")
---
+++ src/supervised.py
@@ -0,0 +1,33 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT))
+
+from src.__config__ import PATHS
+
+DATA_DIR = PATHS.data
+
+class Catapulta:
+    """Classe principal do projeto, responsável por orquestrar o processo de carregamento e limpeza dos dados."""
+    def __init__(self):
+        self.fonts = []
+        self.datasets = {}
+
+    def get_fonts(self) -> list:
+        return self.fonts
+
+    def store_dataset(self, nome: str, dataframe: pd.DataFrame) -> None:
+        if dataframe.empty:
+            print(f"[Aviso] O dataset '{nome}' está vazio. Ele não será salvo.")
+            return
+        
+        if nome in self.datasets:
+            print(f"[Aviso] Substituindo dataset '{nome}' já existente.")
+
+        self.datasets[nome] = dataframe
+
+        if nome not in self.fonts:
+            self.fonts.append(nome)
